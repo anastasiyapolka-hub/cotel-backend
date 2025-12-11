eea698b2896e3c39113945f617a74d373e6fa938
stasybashin <anastasiya.polka@gmail.com>
2025-12-12 00:50:39 +0300

    Initial FastAPI backend

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ openai_client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
